7293357aade66c05976e53b790a04376c722580b
Petra Zemanová <zemape42@gmail.com>
2022-04-28 10:18:20 +0200

    Cafe Lora part 1 solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import './style.css';
 
-console.log('funguju!');
+//toggle navigation icon
+const navElm = document.querySelector('nav');
+const navBtn = document.querySelector('#nav-btn');
+navBtn.addEventListener('click', () => {
+  navElm.classList.toggle('nav-closed');
+});
+
+document
+  .querySelectorAll('nav a')
+  .forEach((navItem) =>
+    navItem.addEventListener('click', () =>
+      navElm.classList.toggle('nav-closed'),
+    ),
+  );
+
+//order drink
+let ordered = false;
+const drinkCup = document.querySelector('.drink__cup');
+const orderBtn = document.querySelector('.order-btn');
+orderBtn.addEventListener('click', () => {
+  if (!ordered) {
+    orderBtn.textContent = 'Zrušit';
+    drinkCup.classList.add('drink__cup--selected');
+    ordered = true;
+  } else {
+    orderBtn.textContent = 'Objednat';
+    drinkCup.classList.remove('drink__cup--selected');
+    ordered = false;
+  }
+});
